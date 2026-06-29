3aff90ed62ef99e21c2986f5f9b1f793192b76a7
rafidoth <rafiulhasan803@gmail.com>
2026-06-29 07:34:44 +0600

    Add scalar openai integration

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,10 +10,10 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
